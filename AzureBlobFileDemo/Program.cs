9bdddb613d72b7bc20fb0e27200d0d883e1b8699
Raju Singh <raju.singh41@hotmail.com>
2024-12-19 17:51:00 +0530

    Add project files.

---
+++ AzureBlobFileDemo/Program.cs
@@ -0,0 +1,86 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+
+namespace AzureBlobFileDemo
+{
+    internal class Program
+    {
+        static   void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            try
+            {
+                var filePath = "C:/Users/DELL/Downloads/sample-4.pdf";
+                var fileName = $"testPdfFile{DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss")}.pdf";
+                byte[] fileBytes = File.ReadAllBytes(filePath);
+                string containerName = "mydemo1";
+
+                // string containerName = "invoices";
+                // string blobStorageConnectionString = "
+                string blobStorageConnectionString = "";
+                CloudStorageAccount StorageAccount = CloudStorageAccount.Parse(blobStorageConnectionString);
+
+                var blobClient = StorageAccount.CreateCloudBlobClient();
+                // Create a container for organizing blobs within the storage account.
+                var container = blobClient.GetContainerReference(containerName);
+                //container.CreateIfNotExists();
+                var directory = container.GetDirectoryReference("Test");
+                var dbFileName = "DefaultDemo/" + fileName;
+                var blob = directory.GetBlockBlobReference(dbFileName);
+
+                //Add content type for blob upload
+
+                //using (Stream _stream = new MemoryStream(fileBytes))
+                //{
+
+                    blob.UploadFromFile(filePath);
+                //}
+
+
+                var sakToken = SasTokenProvider.GetSasToken(blobStorageConnectionString, containerName, "Test/"+dbFileName);
+
+
+                // string token = GetAccessToken(container);
+                //  Console.WriteLine($"{new Uri(blob.Uri, sakToken)}");
+
+                Console.WriteLine(sakToken);
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+       
+
+        private static string GetAccessToken(CloudBlobContainer container)
+        {
+            BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
+
+            containerPermissions.PublicAccess = BlobContainerPublicAccessType.Off;
+           
+            container.SetPermissions(containerPermissions);
+
+            string token = container.GetSharedAccessSignature(new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime = DateTime.MaxValue,
+                Permissions = SharedAccessBlobPermissions.Read,
+            });
+
+            #region Create HrOne Policy if not exists
+            if (!containerPermissions.SharedAccessPolicies.ContainsKey("HrOne"))
+            {
+                containerPermissions.SharedAccessPolicies.Add("HrOne", new SharedAccessBlobPolicy
+                {
+                    SharedAccessExpiryTime = DateTime.MaxValue,
+                    Permissions = SharedAccessBlobPermissions.Read
+                });
+                container.SetPermissions(containerPermissions);
+            }
+            #endregion
+            return token;
+        }
+    }
+}
---
+++ AzureBlobFileDemo/SasTokenProvider.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using System.Net;
+
+namespace AzureBlobFileDemo
+{
+    public static  class SasTokenProvider
+    {
+        public static string GetSasToken(string connectionString, string containerName, string blobName)
+        {
+            var blobServiceClient = new BlobServiceClient(connectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            // set properties on BlobSasBuilder class
+            var sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = containerName,
+                //add IPRange using a string ip address
+                StartsOn = DateTimeOffset.UtcNow,
+                ExpiresOn = DateTime.MaxValue, // Expiration time for the SAS token
+            };
+
+            // set the required permissions on the SAS token
+            sasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Write);
+
+            // add resource specific properties and generate the SAS
+            //if (sasResourceType == SasResourceType.Container)
+            //{
+            //    //Create token at the container level
+            //    sasBuilder.Resource = "c";
+            //    sasToken = containerClient.GenerateSasUri(sasBuilder);
+            //}
+            //else if (sasResourceType == SasResourceType.Blob)
+            //{
+                //Create token at the blob level
+                sasBuilder.Resource = "b";
+                sasBuilder.BlobName = blobName;
+               var sasToken = blobClient.GenerateSasUri(sasBuilder);
+            //}
+            Console.WriteLine($"Generated SAS token: {sasToken}");
+            return sasToken.ToString();
+
+        }
+    }
+}
